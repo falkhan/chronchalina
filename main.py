b4b4f11d7b059e94bd52d061dbaae20fedf4f620
piotrautuch <piotr.autuch@gmail.com>
2022-08-02 22:20:20 +0100

    raise chronchalina from the dead

---
+++ main.py
@@ -0,0 +1,40 @@
+# This example requires the 'message_content' intent.
+
+import discord
+import yaml
+import random
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.messages = True
+
+
+#client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='/')
+
+def load_config():
+    with open('config.yml','r') as stream:
+        config = yaml.safe_load(stream)
+    return config
+
+@bot.event
+async def on_ready():
+    print(f'Chrum chrum witam tu użytkowniczka {bot.user.name}')
+
+
+@bot.command(name='roll',help='Gra dla prawdziwych mężczyzn')
+async def roll(ctx,max=100):
+    print('rolling')
+    try:
+        rolled = random.randrange(1,max)
+    except ValueError:
+        await ctx.send(f'Zjebałeś, *{ctx.author.name}*. Podaj liczbę powyżej 1')
+        return
+
+    if rolled == 1:
+        await ctx.send(f'To twój koniec, *{ctx.author.name}*. Przegranko.')
+    else:
+        await ctx.send(f'Użytkownik *{ctx.author.name}* wyrollował {rolled}!')
+
+config = load_config()
+bot.run(config['token'])
